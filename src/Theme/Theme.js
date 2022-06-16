956c7d5ec04572e21e462e9d983bb851cd486d3f
Qu1etboy <nonzagreanthai@gmail.com>
2022-06-16 21:59:28 +0700

    feat: save theme to localstorage

--- src/Theme/Theme.js
+++ src/Theme/Theme.js
@@ -1,10 +1,18 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({children}) => {
     const defaultTheme = "dark-theme";
-    const [theme, setTheme] = useState(defaultTheme);
+    const [theme, setTheme] = useState(() => {
+        const savedTheme = localStorage.getItem('theme');
+        return savedTheme ? JSON.parse(savedTheme) : defaultTheme;
+    });
+    
+    useEffect(() => {
+        localStorage.setItem('theme', JSON.stringify(theme));
+    }, [theme])
+    
     const toggleTheme = () => {
         setTheme(theme === 'dark-theme' ? 'light-theme' : 'dark-theme');
     }
